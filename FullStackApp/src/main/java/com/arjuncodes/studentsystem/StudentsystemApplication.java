7c268dee996ba1423d508b4f1967ba0305f34996
Said Mohamed <said.mohamed@fewidev.com>
2023-01-06 12:06:58 -0600

    first commit

---
+++ FullStackApp/src/main/java/com/arjuncodes/studentsystem/StudentsystemApplication.java
@@ -0,0 +1,15 @@
+
+package com.arjuncodes.studentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class StudentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsystemApplication.class, args);
+	}
+
+}
