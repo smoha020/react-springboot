27f3a777f9d3ebcccf5425a69fe84535e77d1b52
smoha020 <smoha020@ucr.edu>
2023-01-27 15:16:08 -0600

    ubuntu

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
+apt update -y && apt install openjdk-8-jdk wget -y
+#yum install java-1.8.0-openjdk.x86_64 wget -y   
 mkdir -p /opt/nexus/   
 mkdir -p /tmp/nexus/                           
 cd /tmp/nexus/
