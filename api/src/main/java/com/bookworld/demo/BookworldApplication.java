e3959b5ef9d58bd48c2fd6d2ea3927ef1cc02f56
Said Mohamed <smoha020@ucr.edu>
2023-02-13 17:36:08 -0600

    SpringBootServletInitializer for war file

--- api/src/main/java/com/bookworld/demo/BookworldApplication.java
+++ api/src/main/java/com/bookworld/demo/BookworldApplication.java
@@ -2,9 +2,10 @@ package com.bookworld.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookworldApplication {
+public class BookworldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookworldApplication.class, args);
