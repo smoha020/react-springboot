f6d95e00b27e641ab498cfcc633882d79bf724fe
smoha020 <smoha020@ucr.edu>
2023-02-16 16:33:52 -0600

    install maven via gz

--- server-setup.sh
+++ server-setup.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+
 TOMURL="https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz"
+MVNURL="https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.tar.gz"
+
 yum install java-1.8.0-openjdk -y
-yum install git maven wget -y
+yum install git wget -y
+
+#INSTALL NEWER VERSION MAVEN (AMAZON LINUX MAVEN IS OUTDATED)
+#yum install maven   #THIS WILL GIVE YOU AN OUTDATED VERSION THAT CAN'T BUILD YOUR PROJECT
+
+cd /opt/
+wget $MVNURL -O maven.tar.gz
+MVNTAR=`tar xzvf maven.tar.gz`
+MVNDIR=`echo $MVNTAR | cut -d '/' -f 1`
+ln -s /opt/$MVNDIR /opt/maven
+cat << EOF > /etc/profile.d/maven.sh
+#!/bin/sh
+export M2_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:$PATH
+EOF
+chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+
+
+
+#INSTALL TOMCAT
 cd /tmp/
 wget $TOMURL -O tomcatbin.tar.gz
 EXTOUT=`tar xzvf tomcatbin.tar.gz`
@@ -35,14 +58,23 @@ EOT
 systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
-#git clone https://github.com/smoha020/react-springboot.git
-#cd /home/ec2-user/react-springboot
-#mvn install
+
+
+#CLONE THE REPO
+cd ~
+git clone https://github.com/smoha020/react-springboot.git
+cd react-springboot/api
+
+#BUILD THE ARTIFACT
+mvn install
 systemctl stop tomcat
 sleep 60
+
+#DEPLOY THE WAR FILE TO TOMCAT
 rm -rf /usr/local/tomcat8/webapps/ROOT*
-#cp target/bookworld.war /usr/local/tomcat8/webapps/ROOT.war
-cp /home/ec2-user/bookworld.war /usr/local/tomcat8/webapps/ROOT.war
+cp target/bookworld.war /usr/local/tomcat8/webapps/ROOT.war
+#cp /home/ec2-user/bookworld.war /usr/local/tomcat8/webapps/ROOT.war
+
 systemctl start tomcat
 sleep 120
 #cp /vagrant/application.properties /usr/local/tomcat8/webapps/ROOT/WEB-INF/classes/application.properties
